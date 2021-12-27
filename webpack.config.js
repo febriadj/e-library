fb3ddd462306d85b095278145951d670f1995c7a
febriadj <iamfebriadji@gmail.com>
2021-12-27 16:27:19 +0700

    :wrench: Add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'client/public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|mp4|mp3|waf|ogg)$/,
+        loader: 'file-loader',
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'client/public'),
+    },
+    port: 3000,
+    historyApiFallback: true,
+    hot: true,
+  },
+}
