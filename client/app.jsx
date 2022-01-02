cb168edf348ebf4756488e43012f32d00f1f8047
febriadj <iamfebriadji@gmail.com>
2022-01-02 19:39:54 +0700

    :passport_control: Work on code related to authorization & permissions

--- client/app.jsx
+++ client/app.jsx
@@ -1,28 +1,65 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import 'boxicons';
+import { useDispatch, useSelector } from 'react-redux';
 
 import './styles/app.css';
 import * as page from './pages';
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
+  const isDev = process.env.NODE_ENV === 'development';
+  const dispatch = useDispatch();
+  const { isLoggedIn, user } = useSelector((state) => state);
+
+  const handleGetUser = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      const url = isDev ? 'http://localhost:8000/api/users' : '/api/users';
+
+      const request = await (await fetch(url, {
+        method: 'get',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })).json();
+
+      if (!request.success) throw request;
+
+      dispatch({
+        type: 'counter/user',
+        payload: {
+          data: request.data,
+        },
+      });
+    }
+    catch (error0) {
+      console.error(error0.message);
+    }
+  }
 
   useEffect(() => {
     const token = localStorage.getItem('token');
-    setLoggedIn(!!token);
-  }, [loggedIn]);
+
+    dispatch({
+      type: 'counter/isLoggedIn',
+      payload: {
+        data: !!token,
+      },
+    });
+
+    if (isLoggedIn) handleGetUser();
+  }, [isLoggedIn]);
 
   return (
     <BrowserRouter>
       <Routes>
         {
-          loggedIn ? (
+          isLoggedIn && user ? (
             < >
               <Route exact path="/" element={<page.dashboard />} />
-              <Route exact path="/book" element={<page.bookCatalogs />} />
+              <Route exact path="/book" element={<page.bookCatalog />} />
             </>
-          ) : <Route exact path="/" element={<page.auth setLoggedIn={setLoggedIn} />} />
+          ) : <Route exact path="/" element={<page.auth />} />
         }
       </Routes>
     </BrowserRouter>
