3615777086ed80939dd1b59837edc9183c90d6b5
febriadj <iamfebriadji@gmail.com>
2022-01-02 17:50:47 +0700

    :hammer: Add logout component

---
+++ client/components/logout.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import style from '../styles/components/logout.css';
+
+function Logout({ setLogoutIsOpen }) {
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+
+    setTimeout(() => {
+      window.location.href = '/';
+
+      dispatch({
+        type: 'counter/isLoggedIn',
+        payload: { data: false },
+      });
+
+      dispatch({
+        type: 'counter/user',
+        payload: { data: null },
+      });
+    }, 2000);
+  }
+
+  return (
+    <div className={style.logout}>
+      <div className={style['logout-wrap']}>
+        <div className={style.header}>
+          <h2 className={style.title}>Sign Out</h2>
+          <span className={style.strip}></span>
+          <p className={style.text}>
+            Are you sure you want to sign out of this account?
+            Select 'yes' to continue and select 'cancel' to cancel
+          </p>
+        </div>
+        <div className={style.action}>
+          <button
+            type="submit"
+            className={style.btn}
+            onClick={() => setLogoutIsOpen(false)}
+          >
+            <p className="text">Cancel</p>
+            <box-icon name="x-circle" color="#ffffff"></box-icon>
+          </button>
+          <button
+            type="submit"
+            className={style.btn}
+            onClick={handleLogout}
+          >
+            <p className="text">Yes, I'm sure</p>
+            <box-icon name="check-circle" color="#ffffff"></box-icon>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Logout;
