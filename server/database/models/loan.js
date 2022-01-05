630dc7a7ee70126a3e0bdd3ac9a1309c96b2a7ad
febriadj <iamfebriadji@gmail.com>
2022-01-05 21:36:27 +0700

    :hammer: Add 'loan' database model

---
+++ server/database/models/loan.js
@@ -0,0 +1,74 @@
+const { DataTypes } = require('sequelize');
+const conn = require('../connect');
+
+const UserModel = require('./user');
+const BookCatalogModel = require('./bookCatalog');
+const MemberModel = require('./member');
+
+const LoanModel = conn.define('loans', {
+  userId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  bookCode: {
+    type: DataTypes.STRING(10),
+    allowNull: false,
+  },
+  memberId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+  documentId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    references: {
+      model: 'members',
+      key: 'documentId',
+    },
+  },
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    allowNull: false,
+  },
+  fullname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  bookTitle: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+    allowNull: false,
+  },
+  deadline: {
+    type: DataTypes.DATE,
+    defaultValue: new Date(),
+    allowNull: false,
+  },
+}, {
+  timestamps: true,
+  tableName: 'loans',
+});
+
+const obj = {
+  onUpdate: 'cascade',
+  onDelete: 'cascade',
+}
+
+LoanModel.belongsTo(UserModel, obj);
+
+LoanModel.belongsTo(BookCatalogModel, {
+  ...obj,
+  foreignKey: 'bookCode',
+});
+
+LoanModel.belongsTo(MemberModel, {
+  ...obj,
+  foreignKey: 'documentId',
+});
+
+module.exports = LoanModel;
