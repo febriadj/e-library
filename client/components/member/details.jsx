fa137f9b51517c78cf435d465e2522129317fec8
febriadj <iamfebriadji@gmail.com>
2022-01-07 17:49:45 +0700

    :hammer: Add details & updateMember components in member page

---
+++ client/components/member/details.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import moment from 'moment';
+
+import style from '../../styles/components/member/details.css';
+
+function Details({
+  detailsIsOpen,
+  setDetailsIsOpen,
+  member,
+}) {
+  return (
+    <div className={`${style.details} ${detailsIsOpen && style.active}`}>
+      <div className={style['details-wrap']}>
+        <div className={style.nav}>
+          <div className={style.top}>
+            <button
+              type="button"
+              className={style.btn}
+              onClick={() => setDetailsIsOpen(false)}
+            >
+              <box-icon name="arrow-back"></box-icon>
+            </button>
+            <h2 className={style.title}>Membership Card</h2>
+          </div>
+        </div>
+        <table className={style.table}>
+          <tbody>
+            <tr className={style.row}>
+              <td className={style.column}>Member ID</td>
+              <td className={style.column}>:</td>
+              <td className={style.column}>{member.id}</td>
+            </tr>
+            <tr className={style.row}>
+              <td className={style.column}>Document ID</td>
+              <td className={style.column}>:</td>
+              <td className={style.column}>{member.documentId}</td>
+            </tr>
+            <tr className={style.row}>
+              <td className={style.column}>Full Name</td>
+              <td className={style.column}>:</td>
+              <td className={style.column}>{`${member.firstname} ${member.lastname}`}</td>
+            </tr>
+            <tr className={style.row}>
+              <td className={style.column}>Email Address</td>
+              <td className={style.column}>:</td>
+              <td className={style.column}>{member.email}</td>
+            </tr>
+            <tr className={style.row}>
+              <td className={style.column}>Phone</td>
+              <td className={style.column}>:</td>
+              <td className={style.column}>{member.phone}</td>
+            </tr>
+            <tr className={style.row}>
+              <td className={style.column}>Address</td>
+              <td className={style.column}>:</td>
+              <td className={style.column}>{member.address}</td>
+            </tr>
+            <tr className={style.row}>
+              <td className={style.column}>Join Date</td>
+              <td className={style.column}>:</td>
+              <td className={style.column}>{moment(member.createdAt).format('MMMM DD, YYYY')}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default Details;
