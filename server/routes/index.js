a551a539c1734ee07149c6b7a92466f0813a784e
febriadj <iamfebriadji@gmail.com>
2022-01-07 18:00:49 +0700

    :hammer: Update server routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const authenticate = require('../middleware/auth');
+
 const user = require('../controllers/user');
 const book = require('../controllers/bookCatalog');
+const loan = require('../controllers/loan');
+const member = require('../controllers/member');
 
 router.get('/users', authenticate, user.findOne);
 router.post('/users/login', user.login);
@@ -10,5 +13,14 @@ router.post('/users/register', user.register);
 router.get('/books', authenticate, book.find);
 router.post('/books', authenticate, book.insert);
 router.delete('/books', authenticate, book.delete);
+
+router.get('/loans', authenticate, loan.find);
+router.post('/loans', authenticate, loan.insert);
+router.delete('/loans', authenticate, loan.delete);
+
+router.get('/members', authenticate, member.find);
+router.post('/members', authenticate, member.insert);
+router.delete('/members', authenticate, member.delete);
+router.put('/members', authenticate, member.update);
 
 module.exports = router;
