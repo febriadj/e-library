cfd21ddb66b4e788ed0e875fc1f16695d22a48cc
febriadj <iamfebriadji@gmail.com>
2022-05-04 14:30:40 +0700

    :hammer: Update loan page

--- client/pages/loan.jsx
+++ client/pages/loan.jsx
@@ -7,15 +7,37 @@ import * as comp1 from '../components/loan';
 
 function Loan() {
   const isDev = process.env.NODE_ENV === 'development';
+  const pagLimit = JSON.parse(localStorage.getItem('pag'));
 
   const [logoutIsOpen, setLogoutIsOpen] = useState(false);
   const [loans, setLoans] = useState([]);
   const [addLoanIsOpen, setAddLoanIsOpen] = useState(false);
   const [infoboxData, setInfoboxData] = useState([]);
+
+  const [details, setDetails] = useState({
+    data: null,
+    isOpen: false,
+  });
+
+  const [updateLoan, setUpdateLoan] = useState({
+    data: null,
+    isOpen: false,
+  });
+
   const [params, setParams] = useState({
     page: 1,
-    limit: 10,
     q: '',
+  });
+
+  const [pagInfo, setPagInfo] = useState({
+    start: '',
+    end: '',
+    length: '',
+  });
+
+  const [movePage, setMovePage] = useState({
+    next: null,
+    prev: null,
   });
 
   const handleChart = (args) => {
@@ -52,11 +74,11 @@ function Loan() {
     return chart;
   }
 
-  const handleGetLoans = async () => {
+  const handleGetLoans = async (args) => {
     try {
       const token = localStorage.getItem('token');
-      const { q, page, limit } = params;
-      const url = isDev ? `http://localhost:8000/api/loans?q=${q}&page=${page}&limit=${limit}` : `/api/loans?q=${q}&page=${page}&limit=${limit}`;
+      const { q, page } = params;
+      const url = isDev ? `http://localhost:8000/api/loans?q=${q}&page=${page}&limit=${args}` : `/api/loans?q=${q}&page=${page}&limit=${args}`;
 
       const request = await (await fetch(url, {
         method: 'get',
@@ -66,8 +88,19 @@ function Loan() {
       })).json();
 
       if (!request.success) throw request;
+      const { list, move, info } = request.data;
 
-      setLoans(request.data.list);
+      setLoans(list);
+      setMovePage((prev) => ({
+        ...prev, prev: move.prev, next: move.next,
+      }));
+
+      setPagInfo((prev) => ({
+        ...prev,
+        start: info.start,
+        end: info.end,
+        length: info.length,
+      }));
     }
     catch (error0) {
       console.error(error0.message);
@@ -94,21 +127,52 @@ function Loan() {
     }
   }
 
+  const handlePagEnd = (event) => {
+    localStorage.setItem('pag', JSON.stringify({
+      member: pagLimit.member,
+      book: pagLimit.book,
+      loan: parseInt(event.target.value),
+    }));
+
+    handleGetLoans(event.target.value);
+  }
+
   useEffect(() => {
-    document.title = 'E-Library - Loan'
-    handleGetLoans();
+    document.title = 'E-Library - Loan';
+
     handleInfoboxData();
+    handleGetLoans(pagLimit.loan);
   }, [params]);
 
   return (
     <div className={style.loan}>
       { logoutIsOpen && <comp0.logout setLogoutIsOpen={setLogoutIsOpen} /> }
       <comp0.sidebar linkActive="loan" />
-      <comp1.addLoan
-        setAddLoanIsOpen={setAddLoanIsOpen}
-        addLoanIsOpen={addLoanIsOpen}
-        handleGetLoans={handleGetLoans}
-      />
+      {
+        addLoanIsOpen && (
+          <comp1.addLoan
+            setAddLoanIsOpen={setAddLoanIsOpen}
+            handleGetLoans={handleGetLoans}
+          />
+        )
+      }
+      {
+        updateLoan.isOpen && (
+          <comp1.updateLoan
+            data={updateLoan.data}
+            handleGetLoans={handleGetLoans}
+            setUpdateLoan={setUpdateLoan}
+          />
+        )
+      }
+      {
+        details.isOpen && (
+          <comp1.details
+            details={details}
+            setDetails={setDetails}
+          />
+        )
+      }
       <div className={style['loan-wrap']}>
         <comp0.navbar
           path="Loan"
@@ -124,7 +188,7 @@ function Loan() {
               <input
                 type="text"
                 name="q"
-                placeholder="Enter the title of the book you want to search"
+                placeholder="Enter the member id or code of the book you want to search"
                 onChange={(event) => (
                   setParams((prev) => ({
                     ...prev,
@@ -147,7 +211,57 @@ function Loan() {
             handleGetLoans={handleGetLoans}
             loans={loans}
             handleInfoboxData={handleInfoboxData}
+            setUpdateLoan={setUpdateLoan}
+            setDetails={setDetails}
+            page={params.page}
           />
+          <div className={style.pagination}>
+            <div className={style.move}>
+              <button
+                type="button"
+                className={`${style.btn} ${movePage.prev ? style.active : null}`}
+                onClick={() => {
+                  if (movePage.prev) {
+                    setParams((prev) => ({
+                      ...prev,
+                      page: movePage.prev,
+                    }));
+                  }
+                }}
+              >
+                <box-icon name="chevron-left" color="#ffffff"></box-icon>
+              </button>
+              <button
+                type="button"
+                className={`${style.btn} ${movePage.next ? style.active : null}`}
+                onClick={() => {
+                  if (movePage.next) {
+                    setParams((prev) => ({
+                      ...prev,
+                      page: movePage.next,
+                    }));
+                  }
+                }}
+              >
+                <box-icon name="chevron-right" color="#ffffff"></box-icon>
+              </button>
+            </div>
+            <div className={style.limit}>
+              <select
+                name="limit"
+                className={style['select-limit']}
+                value={pagLimit.loan}
+                onChange={handlePagEnd}
+              >
+                <option value={10}>10</option>
+                <option value={15}>15</option>
+                <option value={20}>20</option>
+                <option value={30}>30</option>
+              </select>
+              <p className={style.text}>rows per page</p>
+            </div>
+            <p className={style.info}>{`${pagInfo.start + 1} - ${pagInfo.end} of ${pagInfo.length}`}</p>
+          </div>
         </div>
       </div>
     </div>
