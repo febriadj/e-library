4c0edb5c865e1986bb2beee6947c61ac021a73bc
febriadj <iamfebriadji@gmail.com>
2022-05-14 15:03:28 +0700

    :hammer: Update bookCatalog/infobox component

--- client/components/bookCatalog/infobox.jsx
+++ client/components/bookCatalog/infobox.jsx
@@ -4,7 +4,9 @@ import style from '../../styles/components/bookCatalog/infobox.css';
 function Infobox({ catalogs }) {
   const handleTotalStock = () => {
     const stock = catalogs.map((item) => item.stock);
-    return stock.length > 0 ? stock.reduce((prev, curr) => prev + curr) : `${0}${0}`;
+    const reduced = stock.length > 0 ? stock.reduce((prev, curr) => prev + curr) : 0;
+
+    return reduced > 0 && reduced < 10 ? `0${reduced}` : reduced;
   }
 
   return (
@@ -12,16 +14,22 @@ function Infobox({ catalogs }) {
       <div className={style.box}>
         <div className={style.content}>
           <p className={style.title}>Total Books</p>
-          <h1 className={style.total}>{catalogs.length < 10 ? `0${catalogs.length}` : catalogs.length}</h1>
+          <h1 className={style.total}>
+            {catalogs.length > 0 && catalogs.length < 10 && 0}{catalogs.length}
+          </h1>
         </div>
-        <box-icon name="book"></box-icon>
+        <span className={style.icon}>
+          <box-icon name="book"></box-icon>
+        </span>
       </div>
       <div className={style.box}>
         <div className={style.content}>
           <p className={style.title}>Total Stock of All Books</p>
           <h1 className={style.total}>{handleTotalStock()}</h1>
         </div>
-        <box-icon name="layer"></box-icon>
+        <span className={style.icon}>
+          <box-icon name="layer"></box-icon>
+        </span>
       </div>
       <div className={style.box}>
         <canvas className={style.canvas} height={150}></canvas>
