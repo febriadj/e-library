ab126ac8c59b32bd8e3023c39d2dc4df65edd245
febriadj <iamfebriadji@gmail.com>
2022-06-04 15:19:51 +0700

    :hammer: Update bookCatalog page

--- client/pages/bookCatalog.jsx
+++ client/pages/bookCatalog.jsx
@@ -134,6 +134,7 @@ function BookCatalogs() {
       <comp0.sidebar linkActive="book" />
       { modal.logout && <comp0.logout setModal={setModal} /> }
       { modal.deleteAccount && (<comp0.deleteAccount setModal={setModal} />) }
+      { modal.changePass && (<comp0.changePass setModal={setModal} />) }
       {
         modal.profile && (
           <comp0.profile
